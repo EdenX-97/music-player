56b72c29330a690364c3a4c6cdd4349f9c79ced2
Mo Xu <edenx97@gmail.com>
2022-12-31 20:59:56 +1100

    first commit

---
+++ Form.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace MusicPlayer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblMusicName = new System.Windows.Forms.Label();
+            this.btnAddMusic = new System.Windows.Forms.Button();
+            this.listBoxMusics = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.remainingTime = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.lblMusicName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnAddMusic, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listBoxMusics, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.mediaPlayer, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(574, 751);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblMusicName
+            // 
+            this.lblMusicName.AutoSize = true;
+            this.lblMusicName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMusicName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblMusicName.Location = new System.Drawing.Point(117, 10);
+            this.lblMusicName.Name = "lblMusicName";
+            this.lblMusicName.Size = new System.Drawing.Size(454, 29);
+            this.lblMusicName.TabIndex = 0;
+            this.lblMusicName.Text = "Music Name";
+            this.lblMusicName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAddMusic
+            // 
+            this.btnAddMusic.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnAddMusic.Location = new System.Drawing.Point(3, 42);
+            this.btnAddMusic.Name = "btnAddMusic";
+            this.btnAddMusic.Size = new System.Drawing.Size(102, 23);
+            this.btnAddMusic.TabIndex = 1;
+            this.btnAddMusic.Text = "Add Music";
+            this.btnAddMusic.UseVisualStyleBackColor = true;
+            this.btnAddMusic.Click += new System.EventHandler(this.btnAddMusic_Click);
+            // 
+            // listBoxMusics
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.listBoxMusics, 3);
+            this.listBoxMusics.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxMusics.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBoxMusics.FormattingEnabled = true;
+            this.listBoxMusics.ItemHeight = 20;
+            this.listBoxMusics.Location = new System.Drawing.Point(3, 71);
+            this.listBoxMusics.Name = "listBoxMusics";
+            this.listBoxMusics.Size = new System.Drawing.Size(568, 629);
+            this.listBoxMusics.TabIndex = 3;
+            this.listBoxMusics.SelectedIndexChanged += new System.EventHandler(this.listBoxMusics_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.btnPlay, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnPause, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnStop, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnPrevious, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnNext, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 706);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(568, 30);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(3, 3);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(101, 24);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(116, 3);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(101, 24);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(229, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(101, 24);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(342, 3);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(101, 24);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(455, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(104, 24);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(3, 13);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(108, 23);
+            this.mediaPlayer.TabIndex = 5;
+            this.mediaPlayer.Visible = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.24229F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.75771F));
+            this.tableLayoutPanel3.Controls.Add(this.remainingTime, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.trackBar, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(117, 42);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(454, 23);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(0, 0);
+            this.trackBar.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar.Maximum = 1000;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(387, 23);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar_MouseDown);
+            this.trackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar_MouseUp);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // remainingTime
+            // 
+            this.remainingTime.AutoSize = true;
+            this.remainingTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.remainingTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.remainingTime.Location = new System.Drawing.Point(387, 0);
+            this.remainingTime.Margin = new System.Windows.Forms.Padding(0);
+            this.remainingTime.Name = "remainingTime";
+            this.remainingTime.Size = new System.Drawing.Size(67, 23);
+            this.remainingTime.TabIndex = 5;
+            this.remainingTime.Text = "0 : 0";
+            this.remainingTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 800);
+            this.MinimumSize = new System.Drawing.Size(600, 800);
+            this.Name = "Form";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblMusicName;
+        private System.Windows.Forms.Button btnAddMusic;
+        private System.Windows.Forms.ListBox listBoxMusics;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label remainingTime;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+
+namespace MusicPlayer
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        // Initialize a list to store url of music file
+        List<String> musicUrls = new List<String>();
+
+        // Initialize number to record how long a song will be, and now time
+        double maxTime, nowTime;
+
+        // Add a new music to play list
+        private void btnAddMusic_Click(object sender, EventArgs e)
+        {
+            // Initialize a new file dialog
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Please choose your mp3 music file";
+            // Define the file type can be choosed
+            fileDialog.Filter = "(*.mp3)|*.mp3";
+            fileDialog.Multiselect = true;
+
+            // When user comfirm choose, add the music
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Receive the selected file names
+                string[] musicList = fileDialog.FileNames;
+
+                // Iterate to save music name
+                foreach (string url in musicList) {
+                    // Remove the prefix of mp3 file
+                    string musicName = Path.GetFileNameWithoutExtension(url);
+                    // Cannot add same music
+                    if (listBoxMusics.Items.Contains(musicName)) continue;
+                    listBoxMusics.Items.Add(musicName);
+                    // Add url to music url list
+                    musicUrls.Add(url);
+                }
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            // If index is larger or equals 0, means now music is pause, we need to continue play it
+            if (listBoxMusics.SelectedIndex >= 0)
+            {
+                mediaPlayer.Ctlcontrols.play();
+                // Enable the timer
+                timer.Enabled = true;
+                // Update track bar immediately
+                updateTrackBar();
+            } else
+            {
+                // If there is not selected music, play first one
+                playNewMusic(0);
+            };
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.pause();
+            // Pause the timer
+            timer.Enabled = false;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.stop();
+            // Pause the timer
+            timer.Enabled = false;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            // Get the index of selected music and minus 1 (previous song)
+            int previousMusicIndex = listBoxMusics.SelectedIndex - 1;
+
+            // Consider edge cases
+            if (previousMusicIndex < 0) return;
+
+            // Play the music
+            playNewMusic(previousMusicIndex);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // Get the index of selected music and plus 1 (next song)
+            int nextMusicIndex = listBoxMusics.SelectedIndex + 1;
+
+            // Consider edge cases
+            if (nextMusicIndex < 0) return;
+
+            // Play the music
+            playNewMusic(nextMusicIndex);
+        }
+
+        // Select a mumsic name and play
+        private void listBoxMusics_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the index of selected music
+            int selectedMusicIndex = listBoxMusics.SelectedIndex;
+
+            // Play the music
+            playNewMusic(selectedMusicIndex);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Every second, update the track bar once
+            updateTrackBar();
+        }
+
+        // Click to drag track bar
+        private void trackBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            // If max time is 0, cannot drag
+            if (maxTime <= 0) return;
+
+            // Pause the timer
+            timer.Enabled = false;
+            // Pause the music
+            mediaPlayer.Ctlcontrols.pause();
+        }
+
+        // Mouse up to set the time of music
+        private void trackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Get the time after drag
+            double nowValue = trackBar.Value;
+            // Set the new time
+            mediaPlayer.Ctlcontrols.currentPosition = nowValue;
+            // Start timer
+            timer.Enabled = true;
+            // Update track bar immediately
+            updateTrackBar();
+            // Play music
+            mediaPlayer.Ctlcontrols.play();
+        }
+
+        // Method to calculate and update track bar
+        private void updateTrackBar()
+        {
+            // Get the time length of the music
+            maxTime = mediaPlayer.currentMedia.duration;
+            // Get current time of music
+            nowTime = mediaPlayer.Ctlcontrols.currentPosition;
+
+            // Update track bar
+            trackBar.Maximum = (int)maxTime;
+            trackBar.Value = (int)nowTime;
+
+            // Calculate remaining time
+            int remainingTimeInt = (int)(maxTime - nowTime);
+            String minutes = (remainingTimeInt / 60).ToString();
+            String seconds = (remainingTimeInt % 60).ToString();
+
+            remainingTime.Text = minutes + " : " + seconds;
+        }
+
+        // Method to play a new music, reuse code
+        private void playNewMusic(int selectedMusicIndex)
+        {
+            // Consider edge cases, not music in list
+            if (musicUrls.Count == 0 || selectedMusicIndex > musicUrls.Count - 1) return;
+
+            // Update select index
+            listBoxMusics.SelectedIndex = selectedMusicIndex;
+
+            // Update music name text
+            lblMusicName.Text = listBoxMusics.SelectedItem.ToString();
+
+            // Get the music url
+            string url = musicUrls[selectedMusicIndex];
+            // Set url of media player and play
+            mediaPlayer.URL = url;
+
+            // Enable the timer
+            timer.Enabled = true;
+            // Update track bar immediately
+            updateTrackBar();
+        }
+    }
+}
